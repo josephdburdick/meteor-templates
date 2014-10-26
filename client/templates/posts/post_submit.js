f3bfc140357b57c1e4c46fccd791b3bdbb118feb
Joe Burdick <josephdburdick@gmail.com>
2014-10-26 17:16:40 -0400

    Demonstrate the order that posts appear using a sleep.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -18,7 +18,8 @@ Template.postSubmit.events({
 				alert('This link has already been posted');
 			}
 
-			Router.go('postPage', {_id: result._id})
+			// Router.go('postPage', {_id: result._id})
+			Router.go('postsList');
 		})
 	}
 });
